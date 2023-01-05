53f8af2f881fb1d368e0d55defd99bb611535666
Kirill Izmikov <65114176+kirill-developer@users.noreply.github.com>
2023-01-05 18:19:57 +0100

    Created Code test project for Backend developer role in Airmine

--- places.py
+++ places.py
@@ -5,8 +5,27 @@ import numpy as np
 import reverse_geocoder as rg
 from geopy.distance import great_circle
 
+#announcing constats
+lat_min, lat_max = -90, 90
+long_min, long_max = -180, 180
+file_path = ('places.csv')
+
+geocoder_name_key = "name"
+geocoder_lat_key = "lat"
+geocoder_lon_key = "lon"
+
+name_key = "Name"
+lat_key = "Latitude"
+lon_key = "Longitude"
+
+someplace_column = "Someplace"
+otherplace_column = "Otherplace"
+distance_column = "Distance, km"
+
+rounding_constant = 1
+
 if __name__ == "__main__":
-# adding if __name__ == "__main__": to avoid reverse_geocoder error
+#adding if __name__ == "__main__": to avoid reverse_geocoder error
 
     # Parse command line arguments
     parser = argparse.ArgumentParser()
@@ -14,26 +33,23 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Read in the input data
-    if args.n:
+    if args.n and args.n > 0:
         # Generate n randomly generated places
         places = []
 
         for i in range(args.n):
             place = {
-                "Name": f"Place {i + 1}",
-                "Latitude": random.uniform(-90, 90),
-                "Longitude": random.uniform(-180, 180)
+                name_key: f"Place {i + 1}",
+                lat_key: random.uniform(lat_min, lat_max),
+                lon_key: random.uniform(long_min, long_max)
             }
             # to make project more fun and interesting I decided to use reverse geocoder to find closest city to random coordinates
-            result = rg.search((place["Latitude"], place["Longitude"]))
-            city = result[0]["name"]
-            lat = result[0]["lat"]
-            lon = result[0]["lon"]
+            result = rg.search((place[lat_key], place[lon_key]))
 
             # Update the place dictionary with the closest city and its coordinates
-            place["Name"] = city
-            place["Latitude"] = lat
-            place["Longitude"] = lon
+            place[name_key] = result[0][geocoder_name_key]
+            place[lat_key] = result[0][geocoder_lat_key]
+            place[lon_key] = result[0][geocoder_lon_key]
 
             places.append(place)
 
@@ -41,7 +57,7 @@ if __name__ == "__main__":
 
     else:
         # Use the places.csv file as input
-        df = pd.read_csv("places.csv")
+        df = pd.read_csv(file_path)
 
     # Print the input data
     print("\nRandom places", "\n", df)
@@ -53,26 +69,26 @@ if __name__ == "__main__":
     for i, place1 in df.iterrows():
         for j, place2 in df.iterrows():
             if i != j:
-                distance = great_circle((place1["Latitude"], place1["Longitude"]),
-                                        (place2["Latitude"], place2["Longitude"])).km
-                distances.append((place1["Name"], place2["Name"], distance))
+                distance = round(great_circle((place1[lat_key], place1[lon_key]),
+                                        (place2[lat_key], place2[lon_key])).km, rounding_constant)
+                distances.append((place1[name_key], place2[name_key], distance))
 
     # making a dataframe of distances for ease of further data processing
-    df_distances = pd.DataFrame(distances, columns=['Place1', 'Place2', 'Distance'])
-    df_distances = df_distances.sort_values(by=['Distance'], ascending=True)
+    df_distances = pd.DataFrame(distances, columns=[someplace_column, otherplace_column, distance_column])
+    df_distances = df_distances.sort_values(by=[distance_column], ascending=True)
     print("\nCalculating distences between places", "\n", df_distances)
     print("\nLength of df with duplicates:", len(df_distances))
 
     # removing mirrowing pairs
     df_distances = df_distances.loc[
-        pd.DataFrame(np.sort(df_distances[['Place1', 'Place2']], 1), index=df_distances.index).drop_duplicates(
+        pd.DataFrame(np.sort(df_distances[[someplace_column, otherplace_column]], 1), index=df_distances.index).drop_duplicates(
             keep='first').index]
     df_distances = df_distances.reset_index(drop=True)
     print("\nDataframe without duplicates", "\n", df_distances)
     print("\nLength of df without duplicates:", len(df_distances))
 
     # finding mean distance and pair with distance closest to the mean
-    mean_distance = df_distances['Distance'].mean().round(3)
-    df_closest = df_distances.iloc[(df_distances['Distance'] - mean_distance).abs().argsort()[:1]]
+    mean_distance = df_distances[distance_column].mean().round(rounding_constant )
+    df_closest = df_distances.iloc[(df_distances[distance_column] - mean_distance).abs().round(rounding_constant).argsort()[:1]]
     df_closest = df_closest.reset_index(drop=True)
     print("\nAverage distance:", mean_distance, "\nClosest pair to the average distance value:\n", df_closest)
